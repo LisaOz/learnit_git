10ba8033e6955a10f089e46282cd3646bb942620
Lisa <frolovaie9@gmail.com>
2022-05-31 14:27:55 +0100

    new chacharters added.

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,8 +4,11 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 
+
+
+
 		
-	System.out.println("Hello World! Hello World!");
+	System.out.println("Hello World!!! Hello World!!!");
 	}
 
 }
